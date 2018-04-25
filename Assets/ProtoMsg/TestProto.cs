ea67a60d22e30190e6bb7d035571f8de52815b2d
MollyWendy <1187820970@qq.com>
2018-04-25 19:02:24 +0800

    protobuf及测试类

---
+++ Assets/ProtoMsg/TestProto.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+   public class TestProto:MonoBehaviour
+    {
+        void Start()
+        {
+            login.msgcharinfo msg = new login.msgcharinfo();
+            msg.name = "Send msg";
+            msg.uaid = 778899;
+        }
+    }
+
